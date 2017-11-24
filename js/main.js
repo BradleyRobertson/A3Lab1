e98ce0a5763a34fedc577d5b8c1b36e53e660ba9
Robertson <b_robertson@fanshawec.ca>
2017-11-24 11:49:56 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 (function () {
-  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   
+
+var Pictures = document.querySelectorAll(".data-ref"),
+Model = document.querySelector(".modelName"),
+Pricing = document.querySelector(".priceInfo"),
+Description = document.querySelector(".modelDetails"),
+appliedClass;
+
+function ChangeElements(){
+let objectIndex = carData[this.id];
+Model.firstChild.nodeValue = objectIndex.Car;
+Pricing.firstChild.nodeValue = objectIndex.Price;
+Description.firstChild.nodeValue = objectIndex.Description;
+
+Pictures.forEach(function(element, index){
+  element.classList.add('nonActive');
+})
+
+this.classList.remove('nonActive');
+
+appliedClass = this.id;
+
+}
+
+
+Pictures.forEach(function(element, index){
+element.addEventListener('click', ChangeElements, false);
+});
 
 })();
